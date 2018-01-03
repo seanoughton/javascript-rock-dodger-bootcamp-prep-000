b351ad2338eca3be8ab6836f56a33714a3c00bdf
Sean Oughton <seanoughton@gmail.com>
2018-01-03 22:05:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -143,9 +143,7 @@ function moveDodger(e) {
       moveDodgerLeft()
     } else if(e.which === RIGHT_ARROW){
       moveDodgerRight()
-    } else if ((e.which !== LEFT_ARROW) && (e.which !== RIGHT_ARROW)){
-      
-    }
+    } 
 
 }
 
